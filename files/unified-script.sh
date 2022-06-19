3c42be5fdb0c4c377cb19200762606f1926f6699
Yillié <yilliee@protonmail.com>
2022-06-19 10:47:59 +0500

    files : unified-script : Add entries for m31

--- files/unified-script.sh
+++ files/unified-script.sh
@@ -23,11 +23,23 @@ load_m21()
     echo "Using default - m21 props"
 }
 
+load_m31()
+{
+    resetprop "ro.product.model" "SM-M315F"
+    resetprop "ro.product.name" "m31nsxx"
+    resetprop "ro.build.product" "m31"
+    resetprop "ro.product.device" "m31"
+    resetprop "ro.vendor.product.device" "m31"
+    echo "Using m31 props"
+
 project=$(getprop ro.bootloader | cut -c1-4 )
 
 case $project in
     "F415")
         load_f41
+        ;;
+    "M315")
+        load_m31
         ;;
     *)
         load_m21
